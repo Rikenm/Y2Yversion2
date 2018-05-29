1e43c5a1f4ebe16fb6c5bf25439cbd485447b113
Riken Maharjan <rikenm@bu.edu>
2018-05-29 14:35:08 -0400

    new login layout

--- app/src/main/java/com/rikenmaharjan/y2yc/activities/BaseFragmentActivity.java
+++ app/src/main/java/com/rikenmaharjan/y2yc/activities/BaseFragmentActivity.java
@@ -2,6 +2,7 @@ package com.rikenmaharjan.y2yc.activities;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -19,6 +20,8 @@ public abstract class BaseFragmentActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setStatusBarColor(Color.WHITE);
         setContentView(R.layout.activity_fragment_base);
 
         FragmentManager fragmentManager = getFragmentManager();
